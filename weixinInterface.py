817d6ad9847c81add2187d78eda6b0d3f68ba14b
lglfa <lglfa666@gmail.com>
2016-09-24 02:56:20 +0800

    update

--- weixinInterface.py
+++ weixinInterface.py
@@ -68,12 +68,8 @@ class WeixinInterface:
             j+=1
             if j>5:
                 break
-        #转义撇号
-        reg = re.compile("""'""")
-        reply_content2 = reg.sub("3", reply_content)
 
-                
-        return self.render.reply_text(fromUser,toUser,int(time.time()),reply_content2)
+        return self.render.reply_text(fromUser,toUser,int(time.time()),reply_content)
 
 
  
